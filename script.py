bcab03be36a67e8be19444920e1dbc12a590028f
dushyanth03 <vvce23ise0208@vvce.ac.in>
2026-03-11 10:38:26 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 def add_numbers(a, b):
-    """Returns the sum of two numbers."""
+    
     return a + b
 def my_function():
-    print("Hello from the Python script!")
+    
 if __name__ == "__main__":
     my_function()
     result = add_numbers(5, 3)
-    print(f"the sum of 5 and 3 is= {result}")
+    print(f"the sum of 5 and 3 is = {result}")
 
