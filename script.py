acefae1275df4aa667ca97d9d0e1a0d0328332cb
dushyanth03 <vvce23ise0208@vvce.ac.in>
2026-03-11 10:34:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,6 +5,6 @@ def my_function():
     print("Hello from the Python script!")
 if __name__ == "__main__":
     my_function()
-    result = add_numbers(10, 5)
-    print(f"The sum of 10 and 5 is: {result}")
+    result = add_numbers(5, 3)
+    print(f"c= {result}")
 
